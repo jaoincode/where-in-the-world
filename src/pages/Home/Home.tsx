814e4d9751fa3b5bf5b5e7ccb313068e1e498577
jaoincode <ojoaogugel@gmail.com>
2023-01-18 10:35:52 -0300

    home page filter options

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,17 @@
-import Header from "../../components/Header";
+import { useEffect, useState } from "react";
+
+type filterOptions =
+  | "all"
+  | "america"
+  | "africa"
+  | "asia"
+  | "oceania"
+  | "antarctica";
 
 function Home() {
+  const [countries, setCountries] = useState<any>([]);
+  const [filter, setFilter] = useState<filterOptions>("all");
+
   return (
     <div className="max-w-sm md:max-w-6xl p-7 md:p-10 mx-auto">
       <div className="flex justify-between flex-col gap-4 px-3 md:flex-row md:gap-0 md:px-0">
@@ -11,13 +22,20 @@ function Home() {
         />
         <select
           id="countries"
-          className="w-full md:w-40  h-10 text-sm font-semibold opacity-90 focus:opacity-100 bg-white dark:bg-slate-700 dark:text-white outline-none px-4"
+          className="w-full md:w-40  h-10 text-sm font-semibold opacity-90 focus:opacity-100 bg-white dark:bg-slate-700 dark:text-white outline-none px-4 rounded-sm"
+          name="regionFilter"
+          value={filter}
+          onChange={({ target }) => setFilter(target.value as filterOptions)}
         >
-          <option selected>Filter by Region</option>
-          <option value="US">United States</option>
-          <option value="CA">Canada</option>
-          <option value="FR">France</option>
-          <option value="DE">Germany</option>
+          <option value="all" disabled>
+            Filter by Region
+          </option>
+          <option value="all">All</option>
+          <option value="america">America</option>
+          <option value="africa">Africa</option>
+          <option value="asia">Asia</option>
+          <option value="oceania">Oceania</option>
+          <option value="antarctica">Antarctica</option>
         </select>
       </div>
     </div>
